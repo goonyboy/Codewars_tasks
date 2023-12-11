56dbbeb31af799e371d216f045fa99bede264b50
Alex Gold <88856796+goonyboy@users.noreply.github.com>
2023-12-11 14:50:55 +0300

    Add files via upload

---
+++ 7kyu/!Can Santa save Christmas.js
@@ -0,0 +1,14 @@
+// Oh no! Santa's little elves are sick this year. He has to distribute the presents on his own.
+
+// But he has only 24 hours left. Can he do it?
+// Your Task:
+
+// You will get an array as input with time durations as string in the following format: HH:MM:SS. 
+// Each duration represents the time taken by Santa to deliver a present. Determine whether he can 
+// do it in 24 hours or not. In case the time required to deliver all of the presents is exactly 24 
+// hours, Santa can complete the delivery ;-) .
+
+function determineTime(durations){
+
+}
+console.log(determineTime(['12:30:00', '09:00:30']))
